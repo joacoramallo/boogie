c5962ac9cd1dcd5bfe92427eb32dc4b5a8894cf1
joacoramallo <81364341+joacoramallo@users.noreply.github.com>
2021-03-25 10:05:30 -0300

    Add files via upload

---
+++ Ramallo_jota2.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ramallo_jota;
+import java.util.Scanner;
+/**
+ *
+ * @author pc02
+ */
+public class Ramallo_jota2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Scanner entrada = new Scanner (System.in);
+    String nombre_alumno;
+    double evaluacion1;
+    double evaluacion2;
+    double evaluacionfinal;
+        System.out.println("Nombre del alumno: ");
+        nombre_alumno = entrada.nextLine();
+        System.out.println("Inserte nota de la 1era evaluación: ");
+        evaluacion1 = entrada.nextDouble();
+        System.out.println("Inserte nota de la 2da evaluación: ");
+        evaluacion2 = entrada.nextDouble();
+        evaluacionfinal = (evaluacion1 + evaluacion2);
+        System.out.println("Nota final: "+evaluacionfinal); 
+    }
+    
+}
