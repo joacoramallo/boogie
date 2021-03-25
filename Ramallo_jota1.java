01c8c01fee65957ee6e248b885a4b48d8f670af4
joacoramallo <81364341+joacoramallo@users.noreply.github.com>
2021-03-25 09:45:40 -0300

    Add files via upload

---
+++ Ramallo_jota1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pc02
+ */
+public class Ramallo_jota1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int a = 10;
+    int b = 5;
+    int suma = a + b;
+    int resta = a - b;
+    int multi = a * b;
+    int divi = a / b;
+        System.out.println("la suma de "+a+" más "+b+" es igual a "+suma);
+        System.out.println("la resta de "+a+" a "+b+" es igual a "+resta);
+        System.out.println("el producto de "+a+" y "+b+" es igual a "+multi);
+        System.out.println("el cociente de "+a+" divido a "+b+" es igual a "+divi);
+
+    }
+    
+}
