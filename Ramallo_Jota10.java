37028f9766a2660732dec0f11361d7f6fb56a57f
joacoramallo <81364341+joacoramallo@users.noreply.github.com>
2021-04-08 09:38:29 -0300

    Add files via upload

---
+++ Ramallo_Jota10.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ramallo_jota;
+
+/**
+ *
+ * @author pc02
+ */
+public class Ramallo_Jota10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("Numeros del 1 al 100");
+    int i=1;
+    do{
+    System.out.println(i);
+    i++; 
+    }
+    while (i<=100);
+}
+}
