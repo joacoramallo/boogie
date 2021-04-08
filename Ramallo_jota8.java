6b6af40ea83a75486b9eb2ce8aa8a3765d3b4862
joacoramallo <81364341+joacoramallo@users.noreply.github.com>
2021-04-08 09:26:56 -0300

    Add files via upload

---
+++ Ramallo_jota8.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author pc02
+ */
+public class Ramallo_jota8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("Numeros del 1 al 100");
+    int i=1;
+    while (i<=100){
+    System.out.println(i);
+    i++;
+    }
+    
+}
+}
