eb787987c1b28e4690ded56911b2ab02bcaf0b07
joacoramallo <81364341+joacoramallo@users.noreply.github.com>
2021-04-22 10:03:02 -0300

    Add files via upload

---
+++ Ramallo_Jota22.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aadesarrollo;
+import java.util.Scanner; 
+/**
+ *
+ * @author pc13
+ */
+public class Ramallo_Jota22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+    int a = 0;
+    System.out.println("Ingrese un numero maximo:");
+    a = entrada.nextInt();
+    int num = 0 ;
+    while (num<=a){
+        if (num%6==0){
+            System.out.println(num);
+        }
+        num++;
+    }
+    }
+    
+}
