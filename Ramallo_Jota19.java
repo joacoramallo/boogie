ec84786fdd0deb7e2449669879644df1e40d0ff1
joacoramallo <81364341+joacoramallo@users.noreply.github.com>
2021-04-22 09:52:28 -0300

    Add files via upload

---
+++ Ramallo_Jota19.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aadesarrollo;
+
+/**
+ *
+ * @author pc13
+ */
+public class Ramallo_Jota19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int num = 0 ;
+    while (num<=2000){
+        if (num%10==0){
+            System.out.println(num);
+        }
+        num++;
+    }
+    }
+}
